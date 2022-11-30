53bc2f7e97f5d7c42d44abd949919f71652df9f8
Mischa Börner <mischa.boerner@gmail.com>
2022-11-30 12:20:42 +0100

    cool

--- js/nav.js
+++ js/nav.js
@@ -1,23 +1,20 @@
-//Das hier ist der Button/Link, der gedrückt wird
+
 let button = document.getElementById("close-button");
 
-//Hier muss die ID des Elements hin, welches aus- und eingeblendet wird
 let nav = document.getElementById("nav");
 
-//Fügt einen Click-Listener hinzu
 button.addEventListener("click", btnClick);
 
 function btnClick() {
-  //style bekommt den aktuellen Wert des Elements(block oder non)
   let style = window.getComputedStyle(nav).height;
     if (style === "0px") {
         nav.style.visibility = "visible";
         nav.style.opacity = "100";
-        nav.style.height = "200px"
+        nav.style.maxHeight = "1000px"
         
     } else {
         nav.style.visibility = "hidden";
         nav.style.opacity = "0";
-        nav.style.height = "0px";
+        nav.style.maxHeight = "0px";
     }
 }
